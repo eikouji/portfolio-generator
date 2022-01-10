674b22dae31435679f21cd80d209890512608225
Eiko Ujifusa <eikorouji@gmail.com>
2022-01-09 18:45:37 -0800

    9.2.4 module lesson situation
    assignment destructuring.

---
+++ app.js
@@ -0,0 +1,13 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+const generatePage = (userName, githubName) => {
+    return `
+      Name: ${userName}
+      GitHub: ${githubName}
+    `;
+};
+
+console.log(generatePage(name, github));
