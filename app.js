7911d85c913df1a966ae63ece48b86ac795630f7
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-06 21:49:55 -0800

    starting at the beginning with this module

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = (userName, githubName) => {
+    return `
+     <!DOCTYPE html>
+     <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      </head>
+
+      <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">GitHub</a></h2>
+      </body>
+      </html>
+    `;
+};
+
+FileSystem.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+// const profileDataArgs
+
+const generatePage = (userName, githubName) => {
+  return `
+    Name: ${userName}
+    GitHub: ${githubName}
+  `;
+};
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const name = profileDataArgs[0];
 const github = profileDataArgs[1];
-
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-};
 
 console.log(generatePage(name, github));
