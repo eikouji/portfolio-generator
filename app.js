f2f7df862ec24055518f66eace6cb1bb18d687c7
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-22 10:59:58 -0700

    first lines of code, node.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const printProfileData = (profileDataArr) => {
+  const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+  };
